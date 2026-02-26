e49acac247ad4b32bd26c703441e91fa18105169
Gautam Srivastava <gsrivastava@gmail.com>
2026-02-26 19:07:49 +0000

    Replace navbar logo with new gold pulse design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,22 @@
 'use client'
 
 import Link from 'next/link'
+import Image from 'next/image'
 
 function NavbarContent() {
   return (
     <nav className="sticky top-0 z-50 bg-[#0B1F21] border-b border-white/5">
       <div className="max-w-[1320px] mx-auto px-4 flex items-center justify-center h-24 relative">
         <Link href="/" className="flex items-center gap-4">
-          <svg width="64" height="64" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M4 16h7l2.5-7 5 14 2.5-7H28" stroke="#C4A23E" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
-          </svg>
+          <Image
+            src="/plhub-logo.png"
+            alt="PLHub"
+            width={72}
+            height={72}
+            priority
+            unoptimized
+            className="h-16 w-auto"
+          />
           <span className="text-5xl font-bold text-white tracking-tight">PLhub</span>
         </Link>
         <a
